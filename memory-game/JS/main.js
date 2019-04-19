92998021752569e9c15825d035c73b5cb0d66676
Ahmed Althagafi <ahmedalthagafi@Ahmeds-MacBook-Pro.local>
2019-04-20 01:59:00 +0300

    created game logic

--- memory-game/JS/main.js
+++ memory-game/JS/main.js
@@ -3,18 +3,30 @@
 var cards= ["queen","queen","king","king"];
 var cardsInPlay = [];
 
-var cardOne = cards[0];
-var cardTwo = cards[2];
-
-cardsInPlay.push(cardOne);
-cardsInPlay.push(cardTwo);
-
-if(cardsInPlay.length === 2){
+function checkForMatch() {
+    if(cardsInPlay.length === 2){
     if(cardsInPlay[0] === cardsInPlay[1]){
 alert("You found a match!");
     }else{
         alert("Sorry, try again.");
     }
+
+
 }
+}
+
+function flipCard(cardId) {
+
+cardsInPlay.push(cards[cardId]);
+checkForMatch();
+console.log("User flipped " + cards[cardId]);
+
+}
+
+
+flipCard(0);
+flipCard(2);
+
+
 
 
