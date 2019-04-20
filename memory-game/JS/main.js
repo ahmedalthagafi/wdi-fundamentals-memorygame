f13f393630b662f637ec3cfb9bdf42b19fa9fe15
Ahmed Althagafi <ahmedalthagafi@Ahmeds-MacBook-Pro.local>
2019-04-20 17:33:16 +0300

    created game logic

--- memory-game/JS/main.js
+++ memory-game/JS/main.js
@@ -26,7 +26,9 @@ var cards = [
 
 var cardsInPlay = [];
 
+
 function checkForMatch() {
+    
     if(cardsInPlay.length === 2){
     if(cardsInPlay[0] === cardsInPlay[1]){
 alert("You found a match!");
@@ -38,9 +40,10 @@ alert("You found a match!");
 }
 }
 
-function flipCard(cardId) {
-
+function flipCard() {
+   cardId = this.getAttribute('data-id');
 cardsInPlay.push(cards[cardId].rank);
+this.setAttribute('src', cards[cardId].cardImage);
 checkForMatch();
 console.log("User flipped " + cards[cardId].rank);
 console.log(cards[cardId].suit);
@@ -49,9 +52,23 @@ console.log(cards[cardId].cardImage);
 
 }
 
+function createBoard(){
 
-flipCard(0);
-flipCard(2);
+    for(i = 0; i < cards.length; i++){
+        var cardElement = document.createElement('img');
+        cardElement.setAttribute('src', "images/back.png");
+        cardElement.setAttribute('data-id', i);
+cardElement.addEventListener('click', flipCard);
+document.getElementById('game-board').append(cardElement);
+
+    }
+   
+}
+
+
+
+
+createBoard();
 
 
 
